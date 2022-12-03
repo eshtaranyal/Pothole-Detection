250ba5efe5794466fa2b5aac6ca84c25691fdadd
eshtaranyal <51694302+eshtaranyal@users.noreply.github.com>
2022-12-03 13:15:13 +0530

    Add files via upload

---
+++ depth_ptcld_.py
@@ -0,0 +1,61 @@
+import sys
+import numpy as np
+import open3d as o3d
+import matplotlib as plt
+import pyransac3d as pyrsc
+import collections
+
+def display_inlier_outlier(cloud, ind):
+    inlier_cloud = cloud.select_by_index(ind)
+    outlier_cloud = cloud.select_by_index(ind, invert=True)
+    print("Showing outliers (red) and inliers (gray): ")
+    outlier_cloud.paint_uniform_color([1, 0, 0])
+    inlier_cloud.paint_uniform_color([0, 0, 1])
+    o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+    
+
+d=[]
+
+pcd_load = o3d.io.read_point_cloud("<path to your point cloud (.ply) file>")
+
+points = np.asarray(pcd_load.points)
+plano1 = pyrsc.Plane()
+length=points.shape[0]
+
+print("Statistical outlier removal")
+cl, ind = pcd_load.remove_statistical_outlier(nb_neighbors=1000, std_ratio=2)
+inlier_cloud = pcd_load.select_by_index(ind)
+o3d.io.write_point_cloud("<file path where new point cloud will be stored>", inlier_cloud)
+
+pcd_load = o3d.io.read_point_cloud("path to inlier cloud(.ply)")
+#display_inlier_outlier(pcd_load, ind)
+points = np.asarray(pcd_load.points)
+length=points.shape[0]
+
+best_eq, best_inliers = plano1.fit(points, 0.005, maxIteration=10000)
+#print(best_inliers)
+nx=best_eq[0]
+ny=best_eq[1]
+nz=best_eq[2]
+
+#print(nx," ,", ny, " , " ,nz)
+for i in range(int(length)):
+	dist=(nx*points[i][0]+ny*points[i][1]+nz*points[i][2])/((nx**2+ny**2+nz**2)**0.5)
+	dist=round(dist, 3)
+	d.append(abs(dist))
+	
+ctr = collections.Counter(d)
+freq=ctr.most_common(1)[0][0]
+least_common = ctr.most_common()[-1]
+index_max_dist=np.where(d == freq)
+
+#print(points[index_max_dist])
+d.sort()
+
+print(d[0]," : ",d[-1])
+print("height : ",abs(d[0]-freq))
+print("depth : ",abs(d[-1]-freq))
+
+plane = pcd_load.select_by_index(best_inliers)
+not_plane = pcd_load.select_by_index(best_inliers, invert=True)
+o3d.visualization.draw_geometries([ plane,not_plane])
